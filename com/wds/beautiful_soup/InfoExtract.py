117303206daee1f8e7ecb661634a9169ed30ca6b
wangdongsong1229 <wangdongsong1229@163.com>
2017-03-10 22:17:22 +0800

    HTML信息提取

---
+++ com/wds/beautiful_soup/InfoExtract.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import requests
+from bs4 import BeautifulSoup
+import re #正则表达式引入
+
+if __name__ == "__main__":
+    r = requests.get("http://python123.io/ws/demo.html")
+    soup = BeautifulSoup(r.text, "html.parser")
+    #所有a标签
+    for link in soup.find_all("a"):
+        print(link.get("href"))
+
+    #查询所有标签
+    for tag in soup.find_all(True):
+        print(tag.name)
+
+    for tag in soup.find_all(re.compile("b")):
+        print(tag.name)
+
+    for tag in soup.find_all("p", "course"):
+        print(tag)
+
+    print(soup.find_all(id="link1"))
+
+    print(soup.find_all(id=re.compile("link")))
+
+    #文本内容包括Basic Python
+    print(soup.find_all(string = "Basic Python"))
+
+    #所有Python字符
+    print(soup.find_all(string = re.compile("python")))
