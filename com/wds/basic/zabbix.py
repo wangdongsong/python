d881015ffbfa14de4d0e2da8e6b5f41b6bb7c3df
wangdongsong1229 <wangdongsong1229@163.com>
2017-03-17 22:22:49 +0800

    commit psutil monitor

--- com/wds/basic/zabbix.py
+++ com/wds/basic/zabbix.py
@@ -2,6 +2,7 @@ import psutil
 import re
 import sys
 from psutil import Process
+from time import sleep
 
 def process_info(procName):
     try:
@@ -15,11 +16,32 @@ def process_info(procName):
         pass
     return None
 
-if __name__ == "__main__":
-
+def print_sys_info():
     proc = process_info("pycharm64.exe")
+    print(proc.environ())
+    print(proc.cpu_percent())
     print(proc.cpu_times())
-    print(proc.memory_info_ex)
-    print(proc.memory_info)
-    print(proc.memory_full_info())
-    print(proc)
+    print(proc.memory_maps())
+    print(proc.num_ctx_switches())
+    print(proc.num_threads())
+    print(proc.num_handles())
+
+if __name__ == "__main__":
+    proc = psutil.Process(8384)
+    #print(proc.name())
+    #print(proc.connections())
+    #for conn in proc.connections():
+       # print(conn)
+    #print(proc.status())
+    #print(proc.io_counters())
+    #print(proc.uids())
+    #print(proc.gids())
+    while(0 == 0):
+        sleep(1)
+        proc = process_info("pycharm64.exe")
+        print(proc.cpu_times)
+    print(proc.cpu_percent())
+    #print(proc.memory_info)
+    #print(proc.memory_percent())
+    #for memmap in proc.memory_maps():
+        #print(memmap)
