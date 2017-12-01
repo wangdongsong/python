bc4d1d15d1b01b9e028d5418b0db4fe460412955
wangdongsong <wangdongsong1229@163.com>
2017-12-01 08:33:52 +0800

    二重积分、高斯积分

--- com/wds/scipy/integrate.py
+++ com/wds/scipy/integrate.py
@@ -15,3 +15,32 @@ result = integrate.quad(lambda x: special.jv(4, x), 0, 20)
 print(result)
 
 print("Gaussian integral", np.sqrt(np.pi), integrate.quad(lambda x: np.exp(-x**2), -np.inf, np.inf))
+
+
+# 函数在积分时需要额外的参数，例如变量进行乘或乘方的因子，那么这些参数可以作为变量传入
+
+def integrand(x, a, b, c):
+    return a*x*x + b*x +c
+
+a = 3
+b = 4
+c = 1
+result = integrate.quad(integrand, 0, np.inf, args=(a, b, c))
+
+#二重积分、三重积分
+
+def integrandl(t, x, n):
+    return np.exp(-x * t) / t ** n
+
+n = 4
+result = integrate.dblquad(lambda t, x: integrandl(t, x, n), 0, np.inf, lambda x:0, lambda x: np.inf)
+
+# 高斯积分
+
+def integrand2(x, a, b):
+    return a *x + b
+
+a = 2
+b = 1
+fixed_result = integrate.fixed_quad(integrand2, 0, 1, args=(a, b))
+result = integrate.quadrature(integrand2, 0, 1, args=(a, b))
