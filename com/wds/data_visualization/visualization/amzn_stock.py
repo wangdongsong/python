63358e807bf16a35b8e35814a59b7f84ea309055
wangdongsong <wangdongsong1229@163.com>
2017-12-03 15:05:49 +0800

    亚马逊股价, matplotlib.cbook包和pylab包绘图

---
+++ com/wds/data_visualization/visualization/amzn_stock.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2017/12/3 15:01
+
+amzn stock 图
+数据源：AMZN.csv
+
+@author: wangdongsong1229@163.com
+"""
+from pylab import plotfile, show, gca
+import os
+import matplotlib.cbook as cbook
+
+
+basePath = os.path.abspath("..")
+dataFilePath = basePath + "/resources/AMZN.csv"
+
+fname = cbook.get_sample_data(dataFilePath, asfileobj=False)
+plotfile(fname, ("date", "high", "low", "close"), subplots=False)
+show()
