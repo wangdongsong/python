991a7f9ef924e82e1548ac573aad8c2cb0d6f32b
wangdongsong <wangdongsong1229@163.com>
2017-12-10 09:23:47 +0800

    《Python数据挖掘概念、方法与实践》 package、练习代码

---
+++ com/wds/data_mining/ReadMe.md
@@ -0,0 +1,5 @@
+# Python数据挖掘概念、方法与实践
+
+* 姚军 译
+
+* 源代码：https://github.com/megansquire/masteringDM
---
+++ com/wds/data_mining/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2017/12/10 9:13
+
+@author: wangdongsong1229@163.com
+"""
+
