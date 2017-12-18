f7f39fef132204e97fb8b0a2b25109e0555019f5
wangdongsong <wangdongsong1229@163.com>
2017-12-18 16:42:57 +0800

    updated

--- com/wds/pandas/datetime_index.py
+++ com/wds/pandas/datetime_index.py
@@ -7,3 +7,26 @@ datetime index
 @author: wangdongsong1229@163.com
 """
 
+import numpy as np
+randn = np.random.randn
+from pandas import *
+from pandas.tseries.offsets import *
+
+start = datetime(2015, 1, 1)
+end = datetime(2015, 1, 1)
+rng = date_range(start, end, freq = "BM")
+
+ts = Series(randn(len(rng)), index=rng)
+print(ts.index)
+ts[:8].index
+ts[::1].index
+  
+#可以直接用日期和字符串作为索引
+ts['8/29/2012']
+ts[datetime(2012, 7, 11):]
+ts["2012"]
+ts["2012-7"]
+
+dft = DataFrame(randn(50000, 1),columns=["X"], index=date_range('20050101', periods=50000, freq='T'))
+print(dft)
+print(dft["2005"])
