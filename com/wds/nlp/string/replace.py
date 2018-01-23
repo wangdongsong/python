83afc4f619a6579ed7faeff24edd518ff5c71641
wangdongsong <wangdongsong1229@163.com>
2018-01-23 19:25:56 +0800

    处理重复字符

--- com/wds/nlp/string/replace.py
+++ com/wds/nlp/string/replace.py
@@ -8,6 +8,8 @@ Created on Mon Jan 22 18:21:06 2018
 """
 
 import re 
+from nltk.corpus import wordnet
+
 replacement_patterns= [
 (r'won\'t', 'will not'),
 (r'can\'t', 'cannot'),
@@ -21,6 +23,9 @@ replacement_patterns= [
 (r'(\w+)\'d', '\g<1> would')
 ]
 
+"""
+正则表达式处理缩略词
+"""
 class RegexpReplacer(object):
     def __init__ (self, patterns=replacement_patterns):
         self.patterns = [(re.compile(regex), repl) for (regex, repl) in patterns]
@@ -30,3 +35,27 @@ class RegexpReplacer(object):
         for (pattern, repl) in self.patterns:
             (s, count) = re.subn(pattern, repl, s)
         return s
+   
+"""
+处理重复字符
+"""
+class RepeatReplacer(object):
+    def __init__(self):
+        self.repeat_regexp = re.compile(r'(\w*)(\w)\2(\2*)')
+        self.repl = r"\1\2\3"
+    
+    def replace(self, word):
+        repl_word = self.repeat_regexp.sub(self.repl, word)
+        if repl_word != word:
+            return self.replace(repl_word)
+        else:
+            return repl_word
+        
+    def replace_wordnet(self, word):
+        if wordnet.synsets(word):
+            return word
+        repl_word = self.repeat_regexp.sub(self.repl, word)
+        if repl_word != word:
+            return self.replace(repl_word)
+        else:
+            return repl_word
