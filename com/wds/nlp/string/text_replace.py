e2c134faa002b76089bb4ff0b6f24dc3008d922a
wangdongsong <wangdongsong1229@163.com>
2018-01-23 17:10:49 +0800

    先替换后切分

--- com/wds/nlp/string/text_replace.py
+++ com/wds/nlp/string/text_replace.py
@@ -13,8 +13,15 @@ from replace import RegexpReplacer
 
 replacer = RegexpReplacer()
 
+#文本替换
 text = replacer.replace("Don't hesitate to ask questions")
 print(text)
-
 text = replacer.replace("She must've gone to the market but she didn't go")
 print(text)
+
+#先替换后切分
+from nltk.tokenize import word_tokenize
+before_text=word_tokenize("Don't hesitate to ask question")
+print(before_text)
+after=word_tokenize(replacer.replace("Don't hesitate to ask question"))
+print(after)
