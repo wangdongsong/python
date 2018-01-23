ba7d1d70e9998d7b91e9e7660c2f53a88f105e56
wangdongsong <wangdongsong1229@163.com>
2018-01-23 11:57:36 +0800

    正则表达式，替换缩略词

--- com/wds/nlp/string/replace.py
+++ com/wds/nlp/string/replace.py
@@ -7,4 +7,26 @@ Created on Mon Jan 22 18:21:06 2018
 @author: wangdongsong1229@163.com
 """
 
+import re 
+replacement_patterns= [
+(r'won\'t', 'will not'),
+(r'can\'t', 'cannot'),
+(r'i\'m', 'i am'),
+('ain\'t', 'is not'),
+(r'(\w+)\'ll', '\g<1> will'),
+(r'(\w+)n\'t', '\g<1> not'),
+(r'(\w+)\'ve', '\g<1> have'),
+(r'(\w+)\'s', '\g<1> is '),
+(r'(\w+)\'re', '\g<1> are'),
+(r'(\w+)\'d', '\g<1> would')
+]
 
+class RegexpReplacer(object):
+    def __init__ (self, patterns=replacement_patterns):
+        self.patterns = [(re.compile(regex), repl) for (regex, repl) in patterns]
+
+    def replace(self, text):
+        s = text
+        for (pattern, repl) in self.patterns:
+            (s, count) = re.subn(pattern, repl, s)
+        return s
