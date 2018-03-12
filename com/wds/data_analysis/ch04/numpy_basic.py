6778a0aa4a7795ca14d02ec0999529804241a9c6
wangdongsong <wangdongsong1229@163.com>
2018-03-12 14:24:29 +0800

    ndarray的数据类型

--- com/wds/data_analysis/ch04/numpy_basic.py
+++ com/wds/data_analysis/ch04/numpy_basic.py
@@ -14,7 +14,9 @@ import numpy as np
 NumPy的ndarray：一种多维数组对象
 """
 
-#ndarry创建
+"""
+ndarry创建
+"""
 
 data = ([0.9, 0.2, 0.8],
         [0.5, -0.1, 0.7])
@@ -39,3 +41,28 @@ print(np.zeros((2, 3)))
 
 #返回未初始化的垃圾值
 print(np.empty((2, 2)))
+
+
+"""
+ndarray的数据类型
+"""
+
+arr1 = np.array([1, 2, 3], dtype = np.float32)
+arr2 = np.array([1, 2, 3], dtype = np.int32)
+print(arr1, arr2)
+
+#类型转换
+arr = np.array([1, 2, 3, 4, 5])
+print(arr.dtype)
+float_arr = arr.astype(np.float64)
+print(float_arr.dtype)
+
+
+#浮点数转换成整数后，则小数部分被截断
+arr = np.array([1.2, 3.3, -2.6])
+print(arr)
+print(arr.astype(np.int32))
+
+#字符串全是数字，则可以转为数值形式
+numeric_strings = np.array(["1.23", "2.6", "-3.1"], dtype = np.string_)
+print(numeric_strings.astype(float))
