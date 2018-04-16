261d0c3a1aafe5b2037df99d4033f7a9118f679a
wangdongsong <wangdongsong1229@163.com>
2018-04-16 18:11:29 +0800

    通过索引方修改数据

--- com/wds/data_analysis/ch05/padas_data_structer.py
+++ com/wds/data_analysis/ch05/padas_data_structer.py
@@ -10,6 +10,10 @@ padas的数据结构介绍
 from pandas import Series, DataFrame
 import pandas as pd
 
+"""
+Series
+"""
+
 obj = Series([4, 7, 0.5, 3])
 #print(obj)
 
@@ -26,28 +30,37 @@ obj2 = Series([4, 7, -1, 3], index = ["a", "b", "c", "d"])
 #print(obj2["a"])
 
 #NumPy运算都会保留索引和值之间的链接
-print(obj2[obj2 > 3])
-print(obj2 * 2)
+#print(obj2[obj2 > 3])
+#print(obj2 * 2)
 
 #Series可以看成一个定长的有序字典
-print("b" in obj2)
-print("e" in obj2)
+#print("b" in obj2)
+#print("e" in obj2)
 
 #可以使用字典创建Series
 sdata = {"oa": 100, "ob":200, "oc": 300, "od":400, "0e": 500}
 obj3 = Series(sdata)
-print(sdata)
+#print(sdata)
 
 #只传入一个字典，则结果Series中的索引就是原字典的键
 #oa有对应的值，其它值为NaN，即缺失值
-states = ["la", "lb", "lc", "oa", "le"]
+states = ["oc", "ob", "lc", "oa", "le"]
 obj4 = Series(sdata, index = states)
-print(obj4)
+#print(obj4)
 
 #缺失值检查
-print(pd.isnull(obj4))
-print(pd.notnull(obj4))
-print(obj4.isnull())
+#print(pd.isnull(obj4))
+#print(pd.notnull(obj4))
+#print(obj4.isnull())
 
 #算术运算中自动对齐不同索引数据
-print(obj3 + obj4)
+#print(obj3 + obj4)
+
+obj4.name = "population"
+obj4.index.name = "state"
+
+print(obj4)
+print("#####")
+#通过索引的赋值方式修改
+obj4.index = ["ac", "ab", "bc", "ba", "le"]
+print(obj4)
