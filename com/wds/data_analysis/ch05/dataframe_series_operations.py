4d2a54b51b2b97fca284413978ff610bcd4f5fd7
wangdongsong <wangdongsong1229@163.com>
2018-04-26 10:15:05 +0800

    updated

--- com/wds/data_analysis/ch05/dataframe_series_operations.py
+++ com/wds/data_analysis/ch05/dataframe_series_operations.py
@@ -73,4 +73,42 @@ data2 = data.drop(["Ohio", "Colorado"])
 
 """
 
+索引、选取和过滤
+
 """
+obj = Series(np.arange(4), index = ["a", "b", "c", "d"])
+#print(obj["a"])
+
+#闭区间
+#print(obj["a":"c"])
+
+#设置值
+obj["a":"b"] = 5
+   
+#DataFrame索引
+data = DataFrame(np.arange(16).reshape((4, 4)), index = ["Ohio", "Colorado", "Utah", "New York"], columns = ["one", "two", "three", "four"])
+print(data)
+#print(data["two"])
+#print(data[["one", "three"]])
+
+#切片方式选取
+#print(data[:2])
+#print(data[data["three"] > 5])
+
+#布尔
+#print(data < 5)
+
+#布尔表达式赋值
+data[data < 5] = 0
+#print(data)
+
+#通过ix的方式切片、索引
+#print(data.ix["Colorado", ["two", "three"]])
+#print(data.ix["Colorado", [1, 2]])
+#print(data.ix[["Colorado", "Utah"], [1, 2]])
+
+#取行索引为2的数据
+print(data.ix[2])
+
+#取three列的值大于的前3列数据
+print(data.ix[data.three > 5, :3])
