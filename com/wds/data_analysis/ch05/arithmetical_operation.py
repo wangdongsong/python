4bd18b02352748a381c0d102b8f03f6f21bf7533
wangdongsong <wangdongsong1229@163.com>
2018-05-02 10:17:26 +0800

    updated

--- com/wds/data_analysis/ch05/arithmetical_operation.py
+++ com/wds/data_analysis/ch05/arithmetical_operation.py
@@ -16,19 +16,24 @@ import numpy as np
 s1 = Series([7.3, -2.5, 3.4, 1.5], index = ["a", "c", "d", "e"])
 s2 = Series([-2.1, 3.6, -1.5, 4, 3.1], index = ["a", "c", "e", "f", "g"])
 
-print(s1 + s2)
+#print(s1 + s2)
 
 df1 = DataFrame(np.arange(9).reshape((3, 3)), columns = list("bcd"), index = ["Ohio", "Texas", "Colorado"])
 df2 = DataFrame(np.arange(12).reshape((4, 3)), columns = list("bde"), index = ["Utah", "Ohio", "Texas", "Oregon"])
 
-print(df1 + df2)
+#print(df1 + df2)
 
 #在算术方法填充值
 
-print(df1.add(df2, fill_value = 0))
-print(df1.reindex(columns = df2.columns, fill_value = 0))
+#print(df1.add(df2, fill_value = 0))
+#print(df1.reindex(columns = df2.columns, fill_value = 0))
 
 
 """
 DataFrame和Series之间的运算
 """
+
+
+#广播计算
+arr = np.arange(12).reshape((3, 4))
+print(arr - arr[0])
