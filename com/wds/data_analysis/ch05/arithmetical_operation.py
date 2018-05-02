1f7880d4a9a9ecac73f3ab130c716f64b5fed0bd
wangdongsong <wangdongsong1229@163.com>
2018-05-02 14:29:21 +0800

    函数的基本运算

--- com/wds/data_analysis/ch05/arithmetical_operation.py
+++ com/wds/data_analysis/ch05/arithmetical_operation.py
@@ -36,4 +36,21 @@ DataFrame和Series之间的运算
 
 #广播计算
 arr = np.arange(12).reshape((3, 4))
-print(arr - arr[0])
+#print(arr - arr[0])
+
+frame = DataFrame(np.arange(12.).reshape((4, 3)), columns = list("bde"), index = ["Utah", "Ohio", "Texas", "Oregon"])
+series = frame.ix[0]
+#print(series)
+
+#默认情况下，DataFrame和Series之间的算术运算会将Series的索引匹配到DataFrame的列
+#print(frame - series)
+
+#如果某个索引值在DataFrame的列或Series的索引中找不到，则参与运算的两个对象就会被重新索引以形成并集
+series2 = Series(range(3), index = ["b", "e", "f"])
+
+#print(frame + series2)
+
+#匹配行且在列上广播，则必须使用算术运算方法
+series3 = frame["d"]
+
+#print(frame.sub(series3, axis = 0))
